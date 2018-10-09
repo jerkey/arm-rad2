c28cfe30bc261f3731ec6a94555ea58212fbea17
Alex Papazoglou <papazoga@gmail.com>
2018-10-08 17:39:56 -0700

    Fix interrupt handler.

--- irq_arm.c
+++ irq_arm.c
@@ -1,12 +1,12 @@
 /*    Function Pointers for Interrupts  */
-typedef void (* tyVctHndlr) (void);
+extern void main_interrupt(void);
 
-tyVctHndlr    IRQ     = (tyVctHndlr)0x0;
-tyVctHndlr    SWI     = (tyVctHndlr)0x0;
-tyVctHndlr    FIQ     = (tyVctHndlr)0x0;
-tyVctHndlr    UNDEF   = (tyVctHndlr)0x0;
-tyVctHndlr    PABORT  = (tyVctHndlr)0x0;
-tyVctHndlr    DABORT  = (tyVctHndlr)0x0;
+void (*IRQ)(void) = main_interrupt;
+void (*SWI)(void) = 0;
+void (*FIQ)(void) = 0;
+void (*UNDEF)(void) = 0;
+void (*PABORT)(void) = 0;
+void (*DABORT)(void) = 0;
 
 __attribute__((interrupt("IRQ"))) void IRQ_Handler(void);
 __attribute__((interrupt("SWI"))) void SWI_Handler(void);
@@ -15,13 +15,13 @@ __attribute__((interrupt("UNDEF"))) void Undef_Handler(void);
 __attribute__((interrupt("ABORT"))) void PAbt_Handler(void);
 __attribute__((interrupt("ABORT"))) void DAbt_Handler(void);
 
-/*__attribute__((interrupt("IRQ"))) void IRQ_Handler(void)
+__attribute__((interrupt("IRQ"))) void IRQ_Handler(void)
 {
 	if ( *IRQ !=0x00)
 	{
 		IRQ();
 	}
-}*/
+}
 
 __attribute__((interrupt("FIQ"))) void FIQ_Handler(void)
 {
